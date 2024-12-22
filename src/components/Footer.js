ad4c52a0fc86d3ccb7e5b4750dd2aaa5e0ea6e3a
GopiChand-N <gopichand5201@gmail.com>
2024-12-22 13:18:53 +0530

    added CN

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
           <h3>Designed and Developed by Gopichand Narisetti</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} GN</h3>
+          <h3>Copyright © {year} CN</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
